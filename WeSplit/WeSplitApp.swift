0977b82570bc081a047b1d44c605eb0e0ffb87e0
Vitor Raimundo <vitorgabinio@gmail.com>
2024-05-06 13:39:44 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Vítor Raimundo on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
