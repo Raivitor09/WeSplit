d35b16667bbe545e9c58624e1095c1555faf6f62
Raivitor09 <vitorgabinio@gmail.com>
2024-05-20 10:55:16 -0300

    Forms and modifying program states

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -1,29 +1,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 1
+    
     var body: some View {
-        let strings = Bundle.main.path(forResource: "Strings", ofType: "plist")
-        let dict = NSDictionary(contentsOfFile: strings!)
-        
-        let helloString = dict?["hello-string"] as! String
-        let introductionString = String(format: (dict?["introduction-string"] as! String), "Vitor")
-        let descriptionString = dict?["description-string"] as! String
-        
-        return VStack(spacing: 10) {
-            Text(helloString)
-                .font(.title.bold())
-            Text(descriptionString)
-                .font(.body)
-            Text(introductionString)
-                .font(.caption)
+        Button("Tap Count: \(tapCount)") {
+            tapCount*=2
         }
     }
 }
 
-#if DEBUG
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+#Preview {
+    ContentView()
 }
-#endif
