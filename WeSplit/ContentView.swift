0bdc967684d585ef99dca7febf498bda496a601d
Raivitor09 <vitorgabinio@gmail.com>
2024-05-27 15:16:26 -0300

    Creating views in Loops

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -1,14 +1,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = 1
+    let students = ["vitor", "rai", "fernandes"]
+    @State private var chooseStudent = "rai"
     
     var body: some View {
-        Button("Tap Count: \(tapCount)") {
-            tapCount*=2
+        NavigationStack {
+            Form {
+                Picker("Choose a Student", selection: $chooseStudent){
+                    ForEach(students, id:\.self) {
+                        Text($0)
+                    }
+                }
+            }
+            .navigationTitle("Choose a student: ")
+            }
         }
     }
-}
+
 
 #Preview {
     ContentView()
